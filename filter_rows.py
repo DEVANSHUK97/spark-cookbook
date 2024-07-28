6aea243ff2326d40291f4349afe7419b7850ed24
Devanshu <devanshukhumrma@gmail.com>
2024-07-28 15:49:25 -0400

    added filter example

--- filter_rows.py
+++ filter_rows.py
@@ -5,6 +5,7 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf().set("spark.master", "local").setAppName("parse_csv")
 sc = SparkContext(conf = conf)
 
+
 def parseLine(line):
     fields = line.split(',')
     field_name_1 = fields[0]
@@ -12,12 +13,12 @@ def parseLine(line):
     field_name_3 = fields[2]
     field_name_4 = fields[3]
     return (field_name_1, field_name_2, field_name_3, field_name_4)
-# sc.textFile(path/to/file.csv) to read the csv file
-lines = sc.textFile("./data/exam_results.csv")
-# map to esolate each comma separated value to its own field
-parsed_lines = lines.map(parseLine)
-# collect to actually carry out the work so the results can be produced to be printed
-results = parsed_lines.collect()
 
+lines = sc.textFile("./data/exam_results.csv")
+parsed_lines = lines.map(parseLine)
+
+filtered_lines = parsed_lines.filter(lambda x: x[3] == 'A')
+
+results = filtered_lines.collect()
 for row in results:
     print(row)
